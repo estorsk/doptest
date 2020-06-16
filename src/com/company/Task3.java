cc4bce3230c187871788371d7746969926820c9c
Dmitriy <estorsk@yandex.ru>
2020-06-16 14:01:19 +0400

    Привел к читабельному виду.

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -2,37 +2,34 @@ package com.company;
 
 public class Task3 {
 
-    public static void run3(){
+    public static void run(){
 
 //      Реализовать алгоритм сортировки пузырьком для сортировки массива.
 
-        int i = 0;
-        int p = 10;
-        int[] arr = new int[p];
-        int n = 0;
-        int t = 0;
-        int j = 0;
+        int size = 5;
+        int[] arr = new int[size];
 
-        for(i = 0; i < arr.length; i ++){
+        for(int i = 0; i < size; i ++){
             arr[i] = (int) Math.round(Math.random() * 9);
         }
 
-        for(j = 0; j < p; j ++) {
-            for (i = n + 1; i < p; i++) {
+        for(int j = 0; j < size; j ++) {
+            int n = 0;
+            for (int i = n + 1; i < size; i++) {
                 for (n = i - 1; n < i; n++) {
                     if (arr[i] < arr[n]) {
-                        t = arr[i];
+                        int t = arr[i];
                         arr[i] = arr[n];
                         arr[n] = t;
                     }
                 }
             }
-            p = p -1;
+            size = size -1;
             n = 0;
-            i = 0;
+            int i = 0;
         }
 
-        for (i = 0; i < arr.length; i ++){
+        for (int i = 0; i < arr.length; i ++){
             System.out.println(arr[i]);
         }
 
