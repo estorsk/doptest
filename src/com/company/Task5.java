1f5bd4fd3516f1d2cddefc87ddd6fbcaede2389a
Dmitriy <estorsk@yandex.ru>
2020-06-22 18:12:06 +0400

    Оптимизировал работу программы строка 29-33.

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -8,6 +8,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Task5 {
 
     public static void run() {
+        
 //    Построй частотный словарь букв русского алфавита.
 //    На вход даем предложение(пример): «Это просто предложение»
 //    На выходе должно вывести в консоль:
@@ -17,17 +18,16 @@ public class Task5 {
 //    ...
 //    я - 1 раз
 
-        String alphabet = "а,б,в,г,д,е,ё,ж,з,и,й,к,л,м,н,о,п,р,с,т,у,ф,х,ц,ч,ш,щ,ъ,ы,ь,э,ю,я";
-        String[] alphabetList = alphabet.split(",");
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] alphabetList = alphabet.toCharArray();
 
         String srt = "Это просто предложение";
-        String[] srtList = srt.split("");
+        char[] srtList = srt.toCharArray();
 
-        for (String elementAlp : alphabetList) {
+        for (char elementAlp : alphabetList) {
             int i = 0;
-            for (String elementStr : srtList) {
-                boolean result = elementAlp.equalsIgnoreCase(elementStr);
-                if (result == true) {
+            for (char elementStr : srtList) {
+                if (elementStr == elementAlp) {
                     i++;
                 }
             }
