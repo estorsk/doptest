7b23518b641bc1431b1426b2da9fdb84eccb22f1
Dmitriy <estorsk@yandex.ru>
2020-06-24 00:40:10 +0400

    Изменил работу задания 6.

--- src/com/company/BaseConverter.java
+++ src/com/company/BaseConverter.java
@@ -3,22 +3,25 @@ package com.company;
 
 public class BaseConverter {
 
-    public static void convert(){
+    public static void convert(String type, Integer value) {
 
 //        Напиши класс BaseConverter для конвертации из градусов по Цельсию в
 //        Кельвины​, ​Фаренгейты​, и так далее. У метода должен быть метод convert, который
 //        и делает конвертацию.
 
-        double tCel = -20;
-
-//        Перевод в Фаренгейты
-        double tFar = (double) 9/5 * tCel + 32;
-        System.out.println("По Фаренгейту = " + tFar);
-
-//        Перевод в Кельвины
-        double tKel = (double) tCel + 273.15;
-        System.out.println("По Кельвину = " + tKel);
-        
+        if(type == "По Цельсию"){
+            System.out.println("По Цельсию = " + value);
+        }else if (type == "По Фаренгейту"){
+            double tFar = (double) 9/5 * value + 32;
+            System.out.println("По Фаренгейту = " + tFar);
+        }  else if (type == "По Кельвину"){
+            double tKel = (double) value + 273.15;
+            System.out.println("По Кельвину = " + tKel);
+        } else if (type == "По Реомюру"){
+            double tReo = (double) value * 0.8;
+            System.out.println("По Реомюру = " + tReo);
+        }
 
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-    BaseConverter.convert();
+        BaseConverter.convert("По Цельсию", 10);
+        BaseConverter.convert("По Фаренгейту", 10);
+        BaseConverter.convert("По Кельвину", 10);
+        BaseConverter.convert("По Реомюру", 17);
 
     }
 }
