a48385ce4a17ec2e1d98d1e5cbafdad40847a0b1
Dmitriy <estorsk@yandex.ru>
2020-06-24 12:56:54 +0400

    Исправил в методе convert void -> double, добавил метод с перечислением typeCurrent.

--- src/com/company/BaseConverter.java
+++ src/com/company/BaseConverter.java
@@ -3,25 +3,30 @@ package com.company;
 
 public class BaseConverter {
 
-    public static void convert(String type, Integer value) {
+    public static double convert(Enum type, Integer value) {
 
 //        Напиши класс BaseConverter для конвертации из градусов по Цельсию в
 //        Кельвины​, ​Фаренгейты​, и так далее. У метода должен быть метод convert, который
 //        и делает конвертацию.
 
-        if(type == "По Цельсию"){
+        if(type == typeCurrent.celsius){
             System.out.println("По Цельсию = " + value);
-        }else if (type == "По Фаренгейту"){
+        }else if (type == typeCurrent.fahrenheit){
             double tFar = (double) 9/5 * value + 32;
             System.out.println("По Фаренгейту = " + tFar);
-        }  else if (type == "По Кельвину"){
+        }  else if (type == typeCurrent.kelvin){
             double tKel = (double) value + 273.15;
             System.out.println("По Кельвину = " + tKel);
-        } else if (type == "По Реомюру"){
+        } else if (type == typeCurrent.reaumur){
             double tReo = (double) value * 0.8;
             System.out.println("По Реомюру = " + tReo);
         }
 
+        return 0;
+    }
+
+    public enum typeCurrent{
+        celsius, fahrenheit, kelvin, reaumur;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BaseConverter.convert("По Цельсию", 10);
-        BaseConverter.convert("По Фаренгейту", 10);
-        BaseConverter.convert("По Кельвину", 10);
-        BaseConverter.convert("По Реомюру", 17);
+        BaseConverter.convert(BaseConverter.typeCurrent.celsius, 10);
+        BaseConverter.convert(BaseConverter.typeCurrent.fahrenheit, 10);
+        BaseConverter.convert(BaseConverter.typeCurrent.kelvin, 10);
+        BaseConverter.convert(BaseConverter.typeCurrent.reaumur, 17);
 
     }
 }
