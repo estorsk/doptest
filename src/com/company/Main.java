0daab86418cb5fbda0830a78c2801daa5922f533
Dmitriy <estorsk@yandex.ru>
2020-06-28 19:58:43 +0400

    Добавил задание 9 и выводы в main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,32 @@
 package com.company;
 
+import javax.swing.text.TabExpander;
+
+import static com.company.Task7.numbers;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        BaseConverter.convert(BaseConverter.TypeCurrent.Celsius, 10);
-        BaseConverter.convert(BaseConverter.TypeCurrent.Fahrenheit, 10);
-        BaseConverter.convert(BaseConverter.TypeCurrent.Kelvin, 10);
-        BaseConverter.convert(BaseConverter.TypeCurrent.Reaumur, 17);
+//        BaseConverter.convert(BaseConverter.TypeCurrent.Celsius, 10);
 
+//        Задание 7
+        System.out.println("Минимальное число = " + Task7.numbers());
+        System.out.println();
+
+//        Задание 8
+        System.out.println("Квадрат+2 = " + Task8.sqrAndTow(3));
+        System.out.println();
+
+//        Задание 8.1
+        Task8.cube(3);
+        System.out.println();
+
+//        Задание 8.2
+        Task8.sum();
+        System.out.println();
+
+//        Задание 9
+        System.out.println("Return = " + Task9.littleBig5(15));
     }
 }
---
+++ src/com/company/Task9.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Task9 {
+
+//    Задача 9:
+//    Напишите метод, который бы возвращал в консоль
+//    true, если число больше 5
+//    и false, если число меньше 5
+
+    public static boolean littleBig5(Integer number){
+
+        if (number > 5){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
